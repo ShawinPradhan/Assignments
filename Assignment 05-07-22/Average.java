e5143e100d10591b4abdd6a7abb6e15d712ff05b
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2022-07-05 18:04:54 +0530

    Update Average.java

--- Assignment 05-07-22/Average.java
+++ Assignment 05-07-22/Average.java
@@ -1,21 +1,24 @@
+//To find the average of 5 subjects
+
 import java.util.Scanner;
 public class average
 {
 	public static void main(String[] args)
 	{
-		int a,b,c,d,e,sum,avg;
+		int sub1,sub2,sub3,sub4,sub5,sum,avg;
 		Scanner obj= new Scanner(System.in);
-		System.out.println("Enter first marks:");
-		a = obj.nextInt();
-		System.out.println("Enter second marks:");
-		b = obj.nextInt();
-		System.out.println("Enter third marks:");
-		c = obj.nextInt();
-		System.out.println("Enter fourth marks:");
-		d = obj.nextInt();
-		System.out.println("Enter fifth marks:");
-		e = obj.nextInt();
-		sum = (a+b+c+d+e);
+		System.out.println("Enter first subject marks:");
+		sub1 = obj.nextInt();
+		System.out.println("Enter second subject marks:");
+		sub2 = obj.nextInt();
+		System.out.println("Enter third subject marks:");
+		sub3 = obj.nextInt();
+		System.out.println("Enter fourth subject marks:");
+		sub4 = obj.nextInt();
+		System.out.println("Enter fifth subject marks:");
+		sub5 = obj.nextInt();
+		sum = (sub1+sub2+sub3+sub4+sub5);
+		System.out.println("Total sum of marks: "+sum);
 		avg = sum/5;
 		System.out.println("The average of 5 subjects is: "+avg);
 	}
