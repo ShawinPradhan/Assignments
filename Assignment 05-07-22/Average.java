1fde1882eb67801a9d1220aea095131201313608
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2022-07-05 16:58:17 +0530

    Update Average.java

--- Assignment 05-07-22/Average.java
+++ Assignment 05-07-22/Average.java
@@ -4,21 +4,17 @@ public class average
 	public static void main(String[] args)
 	{
 		int a,b,c,d,e,sum,avg;
-		Scanner s1= new Scanner(System.in);
+		Scanner obj= new Scanner(System.in);
 		System.out.println("Enter first marks:");
-		a = s1.nextInt();
-		Scanner s2= new Scanner(System.in);
+		a = obj.nextInt();
 		System.out.println("Enter second marks:");
-		b = s2.nextInt();
-		Scanner s3= new Scanner(System.in);
+		b = obj.nextInt();
 		System.out.println("Enter third marks:");
-		c = s3.nextInt();
-		Scanner s4= new Scanner(System.in);
+		c = obj.nextInt();
 		System.out.println("Enter fourth marks:");
-		d = s4.nextInt();
-		Scanner s5= new Scanner(System.in);
+		d = obj.nextInt();
 		System.out.println("Enter fifth marks:");
-		e = s5.nextInt();
+		e = obj.nextInt();
 		sum = (a+b+c+d+e);
 		avg = sum/5;
 		System.out.println("The average of 5 subjects is: "+avg);
