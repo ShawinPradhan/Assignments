efb6d99e569968ad91bc330c75f21c95e1b7cf1b
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2022-07-05 17:06:32 +0530

    Update Area of Rectangle.java

--- Assignment 05-07-22/Area of Rectangle.java
+++ Assignment 05-07-22/Area of Rectangle.java
@@ -3,7 +3,7 @@ public class rect
 {
 	public static void main(String[] args)
 	{
-		int l,b,a;
+		int l,b,a,p;
 		Scanner s= new Scanner(System.in);
 		System.out.println("Enter length.:");
 		l = s.nextInt();
@@ -11,5 +11,7 @@ public class rect
 		b = s.nextInt();
 		a = l*b;
 		System.out.println("Area of Rectangle= "+l+" * "+b+" is: "+a);
+		p = 2*(l + b);
+		System.out.println("Perimeter of Rectangle is: "+p);
 	}
 }
