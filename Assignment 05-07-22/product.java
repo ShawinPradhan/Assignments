b2edf60fd67188ea06658d508d1a28aa5f6ac14a
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2022-07-05 17:03:47 +0530

    Update product.java

--- Assignment 05-07-22/product.java
+++ Assignment 05-07-22/product.java
@@ -4,12 +4,11 @@ public class product
 	public static void main(String[] args)
 	{
 		int n,m,p;
-		Scanner s1= new Scanner(System.in);
+		Scanner s= new Scanner(System.in);
 		System.out.println("Enter a no.:");
-		n= s1.nextInt();
-		Scanner s2= new Scanner(System.in);
+		n= s.nextInt();
 		System.out.println("Enter a no.:");
-		m= s1.nextInt();
+		m= s.nextInt();
 		p = n*m;
 		System.out.println("The product of "+n+" and "+m+" is: "+p);
 	}
