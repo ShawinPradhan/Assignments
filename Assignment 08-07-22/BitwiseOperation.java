7f51f3dec4a881e91be9f43eb0a1475eee5e0c7e
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2022-07-08 16:28:54 +0530

    Create BitwiseOperation.java

---
+++ Assignment 08-07-22/BitwiseOperation.java
@@ -0,0 +1,37 @@
+package basicpackage;
+
+public class BitwiseOperation {
+
+	public static void main(String[] args) {
+		//Bitwise AND operator
+		int x= 16,y=12,z;
+		z=x&y;
+		System.out.println("Value of z:"+z);
+		
+		//Bitwise AND operator
+		int a= 15,b=8,c;
+		c=a&b;
+		System.out.println("Value of c:"+c);
+		
+		//Bitwise OR operator
+		int m= 5,n=2,o;
+		o=m|n;
+		System.out.println("Value of o:"+o);
+		
+		//Bitwise OR operator
+		int d= 13,e=16,f;
+		f=d|e;
+		System.out.println("Value of f:"+f);
+		
+		//Bitwise OR operator
+		int g= 20,h=24,i;
+		i=g|h;
+		System.out.println("Value of i:"+i);
+		
+		//Bitwise AND operator
+		int j= 31,k=16,l;
+		l=j&k;
+		System.out.println("Value of l:"+l);
+	}
+
+}
