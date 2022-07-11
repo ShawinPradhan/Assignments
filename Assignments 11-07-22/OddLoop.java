b0c9221f8b4981180f3c86863d6400cf070767a9
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2022-07-11 17:39:30 +0530

    Update OddLoop.java

--- Assignments 11-07-22/OddLoop.java
+++ Assignments 11-07-22/OddLoop.java
@@ -1,3 +1,4 @@
+//Program to count number of odd numbers in a range
 package basicpackage;
 
 import java.util.Scanner;
@@ -5,17 +6,23 @@ import java.util.Scanner;
 public class OddLoop {
 
 	public static void main(String[] args) {
-		int range, i; //i is the counter variable that will count loop and range is till which number we want to find odd nos.
+		int range, i,count =0; //i is the counter variable that will count loop and range is till which number we want to find odd nos.
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter the range: "); // We need to put the range
 		range = sc.nextInt(); // Scanning the input and storing in variable range
-		System.out.println("The even numbers between 1 to "+range+" is: ");
+		System.out.println("The odd numbers between 1 to "+range+" are: ");
 		for(i=1;i<=range;i++) // Here setting the first integer to be 1 and checking whether the number is less than or equal to range and then incrementing it by 1
 		{
-			if(i%2!=0) // Checking whether the number is divisible by 2 and does not give us a reminder 0 to find odd nos.
-				System.out.println(i);
+			if(i%2!=0)// Checking whether the number is divisible by 2 and does not give us a reminder 0 to find odd nos.
+				{	
+					System.out.println(i);
+					count++;
+					
+				}
 		}
+		System.out.println("The no. of odd numbers are: "+count);
 
 	}
 
 }
+
